771a468306bcf4940a67c42235228002b12c8a00
mycr7 <mytoleh@gmail.com>
2019-11-13 00:12:28 +0300

    Refactoring, no functional changes

--- src/main/java/ru/stqa/yasen/ElementList.java
+++ src/main/java/ru/stqa/yasen/ElementList.java
@@ -14,18 +14,16 @@ public class ElementList extends ForwardingList<ElementInList> {
 
   private ElementListContext listContext;
   private List<ElementInList> items = new ArrayList<>();
-  private Object invalidated;
+  private boolean loaded;
 
   ElementList(ElementContext context, By locator) {
     this.context = context;
     this.locator = locator;
     this.listContext = new ElementListContext(this);
-    System.out.println("Initial invalidated " + invalidated);
   }
 
   void invalidate() {
-    invalidated = null;
-    System.out.println("Setting invalidated to null " + invalidated);
+    loaded = false;
   }
 
   void activate() {
@@ -34,25 +32,21 @@ public class ElementList extends ForwardingList<ElementInList> {
 
   @Override
   protected List<ElementInList> delegate() {
-    System.out.println("Is invalidated " + invalidated);
-    if (invalidated == null) {
+    if (! loaded) {
       List<WebElement> elements = context.findAllBy(locator);
       int toCopy = Math.min(items.size(), elements.size());
       if (items.size() > toCopy) {
-        System.out.println("Deleting extra elements");
         items.subList(toCopy, items.size()).clear();
       }
       for (int i = 0; i < toCopy; i++) {
         items.get(i).element = elements.get(i);
       }
       if (elements.size() > toCopy) {
-        System.out.println("Adding extra elements " + elements.size() + " > " + toCopy);
         for (int i = toCopy; i < elements.size(); i++) {
           items.add(new ElementInList(listContext, elements.get(i), i));
         }
       }
-      invalidated = "OK";
-      System.out.println("List loaded " + invalidated);
+      loaded = true;
     }
     return items;
   }
