767af209db0b989c9fd2bc59646e96a87657d6b4
mycr7 <mytoleh@gmail.com>
2019-11-26 15:03:21 +0300

    Fixing varargs ambiguity (and some other warnings)

--- src/main/java/ru/stqa/yasen/jquery/Select2.java
+++ src/main/java/ru/stqa/yasen/jquery/Select2.java
@@ -38,31 +38,39 @@ public class Select2 extends ElementWrapper {
   }
 
   public Select2 select(Option... options) {
-    element.executeScript("$(arguments[0]).val(arguments[1]).trigger('change');",
-      Arrays.stream(options).map(Option::value).toArray(String[]::new));
+    element.executeScript(
+      "$(arguments[0]).val(arguments[1]).trigger('change');",
+       Arrays.stream(options).map(Option::value).toArray());
     return this;
   }
 
   public Select2 selectByValue(String... values) {
-    element.executeScript("$(arguments[0]).val(arguments[1]).trigger('change');", values);
+    element.executeScript(
+      "$(arguments[0]).val(arguments[1]).trigger('change');",
+       Arrays.asList(values).toArray());
     return this;
   }
 
   public Select2 selectByText(String... texts) {
     String script = "var texts = arguments[1], values = [], adapter = $(arguments[0]).data().select2.dataAdapter;"
-       + "$(arguments[0]).find('option').each(function() { var item = adapter.item($(this)); if (texts.indexOf(item.text) >= 0) { values.push(item.id) } });"
-       + "$(arguments[0]).val(values).trigger('change');";
-    element.executeScript(script, texts);
+      + "$(arguments[0]).find('option').each(function() { var item = adapter.item($(this)); if (texts.indexOf(item.text) >= 0) { values.push(item.id) } });"
+      + "$(arguments[0]).val(values).trigger('change');";
+    element.executeScript(script, Arrays.asList(texts).toArray());
     return this;
   }
 
+  @SuppressWarnings("unchecked")
   public List<Option> allOptions() {
-    return ((List<Map<String, Object>>) element.executeScript("var data = [], adapter = $(arguments[0]).data().select2.dataAdapter; $(arguments[0]).find('option').each(function() { data.push(adapter.item($(this))) }); return data"))
+    String script = "var data = [], adapter = $(arguments[0]).data().select2.dataAdapter;"
+      + "$(arguments[0]).find('option').each(function() { data.push(adapter.item($(this))) });"
+      + "return data;";
+    return ((List<Map<String, Object>>) element.executeScript(script))
       .stream()
       .map(props -> new Option((String) props.get("id"), (String) props.get("text")))
       .collect(Collectors.toList());
   }
 
+  @SuppressWarnings("unchecked")
   public List<Option> selectedOptions() {
     return ((List<Map<String, Object>>) element.executeScript("return $(arguments[0]).select2('data')"))
       .stream()
